d1f54835c50a5f686094c299418cd882c2e7b805
luis <luiss18.code@gmail.com>
2025-04-30 00:31:38 -0300

    feat: Atualiza configuração do banco de dados para usar variáveis de ambiente

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -1,11 +1,18 @@
 import mysql from 'mysql'
 
+const PASSWORD = process.env.SQL_PASSWORD
+const NAME = process.env.SQL_NAME_DATABASE
+const PORT = process.env.SQL_PORT
+const USER = process.env.SQL_USER
+const HOST = process.env.SQL_HOST
+
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'food-list'
+    host: HOST,
+    port: PORT,
+    user: USER,
+    password: PASSWORD,
+    database: NAME
 })
 
 /**
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from './app.js'
 
 const PORT = 3000
 
-app.listen(PORT, () => {
+app.listen(PORT || process.env.PORT, () => {
     console.log(`Server rodando na porta http://localhost:${PORT}`)
 })
 
